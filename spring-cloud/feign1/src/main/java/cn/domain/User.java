98e56c43bd3f8ffb236ce150f26e480647afab61
best <M__Java@163.com>
2020-04-13 15:16:18 +0800

    no message

---
+++ spring-cloud/feign1/src/main/java/cn/domain/User.java
@@ -0,0 +1,46 @@
+package cn.domain;
+
+import java.util.Objects;
+
+public class User {
+    String name;
+    Integer age;
+
+
+    public User() {
+    }
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return Objects.equals(this.toString(), o.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
